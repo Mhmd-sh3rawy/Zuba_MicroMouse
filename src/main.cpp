8b02f18f2c41d014f92d999ca668e1f16b56633f
sh3rawy <mhmdelbaset404@gmail.com>
2025-09-11 16:30:54 +0300

    add IR draft

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,42 @@
 #include <Arduino.h>
 
-const int ulmPIn = 0;
-const int reciever = 12; 
-const int IRloadTime = 25;
-int IRraw_read = 0;
+#define RECEIVER_IR 12
+#define EMMITTER_PIN 36
+int reading = 0;
 
+#define FREQ 5000 
+#define DUTY_CYCLE 64
+#define CHANNEL 0
+#define RESOLUTION 8 
+
+unsigned long irReload = 125;
+
+void readIR();
+void enEmmitter();
 
 void setup(){ 
-  pinMode(ulmPIn, OUTPUT);
-  pinMode(reciever, INPUT);
-  Serial.begin(115200);  
+  Serial.begin(115200);
+  pinMode(RECEIVER_IR, INPUT);
+  pinMode(EMMITTER_PIN, OUTPUT);
+  ledcSetup(CHANNEL, FREQ, RESOLUTION);
+  ledcAttachPin(EMMITTER_PIN, CHANNEL); 
 }
 
-
-void loop(){ 
-  IRraw_read = analogRead(reciever);
-  Serial.println(IRraw_read);
-
+void loop(){
+  readIR();
 }
 
-void rawReadDebounced(){
+void readIR(){
   unsigned long current_time = millis();
 
-  if(millis() - current_time >= IRloadTime){
-    IRraw_read = analogRead(reciever);
+  if(millis() - current_time >= irReload){
+    reading = analogRead(RECEIVER_IR);
+    current_time = millis();
+    Serial.println(reading); 
   }
 
+}
+
+void enEmmitter(){ 
+  ledcWrite(CHANNEL, 64);
 }
